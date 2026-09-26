daf114560b36da1023aef2e6fff4d819101cd791
jfbnls <120685626+jfbnls@users.noreply.github.com>
2026-09-26 09:46:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pese-plastique-v1';
+const CACHE = 'pese-plastique-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => {
